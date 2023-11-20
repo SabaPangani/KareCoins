dedbdd8571ba058671ad7980ad8f120a752bb3e4
SabaPangani <Spangani37@gmail.com>
2023-11-20 12:26:37 +0400

    Updated navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,28 @@
-import moneySend from "../assets/money-send.svg"
-import dep from "../assets/data.svg"
-import users from "../assets/user-square.svg"
+import moneySend from "../assets/money-send.svg";
+import dep from "../assets/data.svg";
+import users from "../assets/user-square.svg";
+import { NavLink } from "react-router-dom";
 export default function () {
   return (
-      <nav className="w-[316px]">
-        <div className="w-[110%] h-36 fixed right-1/2 bottom-[-3rem] translate-x-1/2 bg-white flex flex-row gap-x-20 justify-center items-center rounded-[48%]">
+    <nav className="w-[316px]">
+      <div
+        className="w-[110%] h-36 fixed right-1/2 bottom-[-3rem] translate-x-1/2 bg-white flex flex-row gap-x-20 justify-center items-center rounded-[48%]"
+        id="nav"
+      >
+        <NavLink
+          to="/departments"
+          className={({ isActive }) => [isActive ? "active" : ""].join(" ")}
+        >
           <img className="h-[26px] w-[26px] mb-14" src={dep} alt="" />
-          <img className="h-10 w-10 mb-16" src={moneySend} alt="" />
-          <img className="h-[26px] w-[26px] mb-14" src={users} alt="" />
-        </div>
-      </nav>
+        </NavLink>
+        <img className="h-10 w-10 mb-16 z-10" src={moneySend} alt="" />
+        <NavLink
+          to="/users"
+          className={({ isActive }) => [isActive ? "active" : ""].join(" ")}
+        >
+          <img className="h-[26px] w-[26px] mb-14 z-10" src={users} alt="" />
+        </NavLink>
+      </div>
+    </nav>
   );
 }
